e09d94089fe46f8532cbed47ea14543bec2604a3
Karel Prchal <prchal.karel@outlook.com>
2021-03-09 22:31:04 +0100

    Added simple Csharp code

---
+++ Src/Dankov.Core/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using UnrealEngine.Framework;
+
+namespace Dankov.Core
+{
+    public static class Main
+    {
+        public static void OnWorldBegin() => Debug.AddOnScreenMessage(-1, 10.0f, Color.Red, nameof(OnWorldBegin));
+
+        public static void OnWorldPostBegin() => Debug.AddOnScreenMessage(-1, 10.0f, Color.Blue, nameof(OnWorldBegin));
+
+        public static void OnWorldEnd() => Debug.AddOnScreenMessage(-1, 10.0f, Color.Green, nameof(OnWorldEnd));
+
+        public static void OnWorldPrePhysicsTick(float deltaTime) => Debug.AddOnScreenMessage(1, 10.0f, Color.Yellow, $"{nameof(OnWorldPostPhysicsTick)}-{DateTime.Now}");
+
+        public static void OnWorldDuringPhysicsTick(float deltaTime) => Debug.AddOnScreenMessage(2, 10.0f, Color.Black, $"{nameof(OnWorldDuringPhysicsTick)}-{DateTime.Now}");
+
+        public static void OnWorldPostPhysicsTick(float deltaTime) => Debug.AddOnScreenMessage(3, 10.0f, Color.White, $"{nameof(OnWorldPostPhysicsTick)}-{DateTime.Now}");
+
+        public static void OnWorldPostUpdateTick(float deltaTime) => Debug.AddOnScreenMessage(4, 10.0f, Color.Pink, $"{nameof(OnWorldPostUpdateTick)}-{DateTime.Now}");
+    }
+}
